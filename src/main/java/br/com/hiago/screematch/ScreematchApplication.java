ed8a8e0f180b50806f468e5cb69988b8c32341a2
HiagoAlvesdasilva <hiagoalvesdasilva11@gmail.com>
2025-05-19 15:59:12 -0300

    inicio do projeto spring

---
+++ src/main/java/br/com/hiago/screematch/ScreematchApplication.java
@@ -0,0 +1,27 @@
+package br.com.hiago.screematch;
+
+import br.com.hiago.screematch.model.DadosSerie;
+import br.com.hiago.screematch.service.ConsumoApi;
+import br.com.hiago.screematch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreematchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreematchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("http://www.omdbapi.com/?t=lost&apikey=74979642");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+
+}
---
+++ src/main/java/br/com/hiago/screematch/service/IConverteDados.java
@@ -0,0 +1,5 @@
+package br.com.hiago.screematch.service;
+
+public interface IConverteDados {
+    <T> T obterDados(String json, Class<T> classe);
+}
