d4618adcb44eb729149787646663cb2391b96a0f
HiagoAlvesdasilva <hiagoalvesdasilva11@gmail.com>
2025-05-23 15:58:18 -0300

    feat: criado classe  Episodio

---
+++ src/main/java/br/com/hiago/screematch/model/Episodio.java
@@ -0,0 +1,80 @@
+package br.com.hiago.screematch.model;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+public class Episodio {
+    private Integer tempodada;
+    private String titulo;
+    private Integer numeroEpisodio;
+    private Double avaliacao;
+    private LocalDate dataLancamento;
+
+    public Episodio(Integer numeroTemporada, DadosEpisodios dadosEpisodios) {
+        this.tempodada = numeroTemporada;
+        this.titulo = dadosEpisodios.titulo();
+        this.numeroEpisodio = dadosEpisodios.numeroEpisodio();
+
+        try {
+            this.avaliacao = Double.valueOf(dadosEpisodios.avaliacao());
+        } catch (NumberFormatException ex){
+            this.avaliacao = 0.0;
+        }
+
+        try {
+            this.dataLancamento = LocalDate.parse(dadosEpisodios.dataLancamento());
+        }catch (DateTimeParseException ex){
+            this.dataLancamento = null;
+        }
+    }
+
+    public Integer getTempodada() {
+        return tempodada;
+    }
+
+    public void setTempodada(Integer tempodada) {
+        this.tempodada = tempodada;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getNumeroEpisodio() {
+        return numeroEpisodio;
+    }
+
+    public void setNumeroEpisodio(Integer numeroEpisodio) {
+        this.numeroEpisodio = numeroEpisodio;
+    }
+
+    public Double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(Double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "tempodada=" + tempodada +
+                ", titulo='" + titulo + '\'' +
+                ", numeroEpisodio=" + numeroEpisodio +
+                ", avaliacao=" + avaliacao +
+                ", dataLancamento=" + dataLancamento ;
+    }
+}
