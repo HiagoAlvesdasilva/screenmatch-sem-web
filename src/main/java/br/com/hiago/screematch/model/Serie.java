2f667ae4f5677b96a320ddf353962e63dcf49896
HiagoAlvesdasilva <hiagoalvesdasilva11@gmail.com>
2025-06-20 21:42:14 -0300

    feat: update Classe Serie
    adicionada as anotaçoes para usar a classe como uma entidade para persistir no BD

--- src/main/java/br/com/hiago/screematch/model/Serie.java
+++ src/main/java/br/com/hiago/screematch/model/Serie.java
@@ -2,18 +2,39 @@ package br.com.hiago.screematch.model;
 
 import br.com.hiago.screematch.model.enums.Categoria;
 import br.com.hiago.screematch.service.traducao.ConsultaMyMemory;
+import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.OptionalDouble;
 
+@Entity
+@Table(name = "tb_series")
 public class Serie {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
     private String titulo;
+
     private Integer totalTemporadas;
+
     private Double avaliacao;
+
+    @Enumerated(EnumType.STRING)
     private Categoria genero;
+
     private String atores;
+
     private String poster;
+
     private String sinopse;
+
+    @Transient
+    private List<Episodio> episodios = new ArrayList<>();
+
 
     public Serie(DadosSerie dadosSerie){
         this.titulo = dadosSerie.titulo();
@@ -23,6 +44,23 @@ public class Serie {
         this.atores = dadosSerie.atores();
         this.poster = dadosSerie.poster();
         this.sinopse = ConsultaMyMemory.obterTraducao(dadosSerie.sinopse().trim());
+    }
+
+    @Transient
+    public List<Episodio> getEpisodios() {
+        return episodios;
+    }
+
+    public void setEpisodios(List<Episodio> episodios) {
+        this.episodios = episodios;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitulo() {
