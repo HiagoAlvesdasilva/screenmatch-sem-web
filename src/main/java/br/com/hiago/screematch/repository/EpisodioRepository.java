5a998d51d471fc4a33a1dcd56667ff90960271dc
HiagoAlvesdasilva <hiagoalvesdasilva11@gmail.com>
2025-06-28 01:39:45 -0300

    fix: EpisodioRepository
    adicionado anotação do Repositorio

--- src/main/java/br/com/hiago/screematch/repository/EpisodioRepository.java
+++ src/main/java/br/com/hiago/screematch/repository/EpisodioRepository.java
@@ -2,6 +2,8 @@ package br.com.hiago.screematch.repository;
 
 import br.com.hiago.screematch.model.Episodio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EpisodioRepository extends JpaRepository<Episodio, Long> {
 }
