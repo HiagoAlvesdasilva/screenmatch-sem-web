b956c6c1eec4ceedf7879969f4e4342b24b8b7f4
HiagoAlvesdasilva <hiagoalvesdasilva11@gmail.com>
2025-06-28 01:40:36 -0300

    feat: SerieRepository
    incluido queries derivadas para top 5, categoria e avaliação por temporadas

--- src/main/java/br/com/hiago/screematch/repository/SerieRepository.java
+++ src/main/java/br/com/hiago/screematch/repository/SerieRepository.java
@@ -1,6 +1,7 @@
 package br.com.hiago.screematch.repository;
 
 import br.com.hiago.screematch.model.Serie;
+import br.com.hiago.screematch.model.enums.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -15,4 +16,9 @@ public interface SerieRepository extends JpaRepository <Serie, Long> {
 
     List<Serie> findByAvaliacaoGreaterThanEqualOrderByAvaliacaoDesc(Double avaliacao);
 
+    List<Serie> findTop5ByOrderByAvaliacaoDesc();
+
+    List<Serie> findByGenero(Categoria genero);
+
+    List<Serie> findByTotalTemporadasAndAvaliacaoGreaterThanEqualOrderByAvaliacaoDesc(Integer temporadas, Double avaliacao);
 }
